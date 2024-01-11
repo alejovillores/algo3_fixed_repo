71642382b1f3715c81248cb92687d8838a4a7389
alejovillores <avillores@fi.uba.ar>
2024-01-10 23:37:22 -0300

    Agrego multiples personajes

--- src/main/java/edu/fiuba/algo3/modelo/RickAndMortyApi.java
+++ src/main/java/edu/fiuba/algo3/modelo/RickAndMortyApi.java
@@ -7,8 +7,19 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
+/*
+*  Esta clase encapsula la interaccion con la API de Rick and Morty
+*  - La documentacion de la API se puede encontrar en https://rickandmortyapi.com/documentation
+*
+*  - La documentacion sobre los objetos java.net.http se pueden encontrar en
+*  https://docs.oracle.com/en/java/javase/11/docs/api/java.net.http/java/net/http/package-summary.html
+*/
 public class RickAndMortyApi {
 
+    /*
+    * Realiza una request por un solo personaje cuyo id coincide con idPersonaje
+    * Documentacion: https://rickandmortyapi.com/documentation/#get-a-single-character
+    * */
     public HttpResponse<String> obtenerPersonaje(int idPersonaje) {
         try {
             HttpRequest request = HttpRequest.newBuilder()
@@ -19,6 +30,29 @@ public class RickAndMortyApi {
 
             return client.send(request, HttpResponse.BodyHandlers.ofString());
 
+        } catch (URISyntaxException | InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /*
+     * Realiza una request obteniendo multiples personajes
+     * Documentacion: https://rickandmortyapi.com/documentation/#get-multiple-characters
+     * */
+    public HttpResponse<String> obtenerPersonajes(int[] idPersonajes) {
+        try {
+            String url = "https://rickandmortyapi.com/api/character/";
+            for (int i = 0; i < idPersonajes.length; i++) {
+                url = url.concat(String.format("%d,",idPersonajes[i]));
+            }
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .GET()
+                    .build();
+            HttpClient client = HttpClient.newBuilder().build();
+
+            return client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (URISyntaxException | InterruptedException | IOException e) {
             throw new RuntimeException(e);
         }
--- src/main/test/edu/fiuba/algo3/architecture/RickAndMortyApiTest.java
+++ src/main/test/edu/fiuba/algo3/architecture/RickAndMortyApiTest.java
@@ -1,14 +1,13 @@
 package edu.fiuba.algo3.architecture;
 
 import edu.fiuba.algo3.modelo.RickAndMortyApi;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.IOException;
-import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
+import org.junit.jupiter.api.Test;
+
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.net.http.HttpResponse;
 
 public class RickAndMortyApiTest {
@@ -21,7 +20,45 @@ public class RickAndMortyApiTest {
         HttpResponse<String> response = api.obtenerPersonaje(1);
 
         // Assert
-        int HTTP_OK = 200;
         assertEquals(response.statusCode(),HTTP_OK);
+    }
+
+    @Test
+    public void PersonajeTieneIdCorrecto(){
+        // Arrange
+        RickAndMortyApi api = new RickAndMortyApi();
+
+        // Act
+        HttpResponse<String> response = api.obtenerPersonaje(1);
+
+        // Assert
+        assertTrue(response.body().contains("\"id\":1"));
+    }
+
+    @Test
+    public void ListaDeDosPersonajes(){
+        // Arrange
+        RickAndMortyApi api = new RickAndMortyApi();
+        int[] idPersonajes = new int[]{ 1,2};
+
+        // Act
+        HttpResponse<String> response = api.obtenerPersonajes(idPersonajes);
+
+        // Assert
+        assertEquals(response.statusCode(),HTTP_OK);
+    }
+
+    @Test
+    public void PersonajesTienenIdCorrectos(){
+        // Arrange
+        RickAndMortyApi api = new RickAndMortyApi();
+        int[] idPersonajes = new int[]{ 1,2};
+
+        // Act
+        HttpResponse<String> response = api.obtenerPersonajes(idPersonajes);
+
+        // Assert
+        assertTrue(response.body().contains("\"id\":1"));
+        assertTrue(response.body().contains("\"id\":2"));
     }
 }
